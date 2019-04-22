5b70845ca29651e3a61d88773d55b5a5cecbbf3b
NXXR <thenexxorproject@gmail.com>
2019-04-22 11:04:19 +0900

    solidified pcreate

--- docs/ProjectReports/WeeklyReports/pcreate.py
+++ docs/ProjectReports/WeeklyReports/pcreate.py
@@ -4,5 +4,18 @@ import sys
 import os
 import glob
 
-file = glob.glob("Week"+sys.argv[1]+"*.md")
-os.system("pandoc -t slidy -s "+file[0]+" -o "+file[0][:-3]+".html --self-contained")
+if len(sys.argv) > 1:
+    for arg in sys.argv[1:]:
+        if os.path.isfile(sys.argv[1]):
+            os.system("pandoc -t slidy -s " + sys.argv[1] + " -o " + sys.argv[1][:-3] + ".html --self-contained")
+        elif len(sys.argv[1]) == 2:
+            number = sys.argv[1]
+            file = glob.glob("Week" + sys.argv[1] + "*.md")
+            os.system("pandoc -t slidy -s " + file[0] + " -o " + file[0][:-3] + ".html --self-contained")
+        else:
+            print("invalid argument \""+arg+"\" skipping...")
+else:
+    number = input("Week Number (2 digits): ")
+    if len(number) == 2 and number.isdigit():
+        file = glob.glob("Week" + sys.argv[1] + "*.md")
+        os.system("pandoc -t slidy -s " + file[0] + " -o " + file[0][:-3] + ".html --self-contained")
