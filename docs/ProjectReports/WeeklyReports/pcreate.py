b4a6e54ad1c6c0ce85cc17616c6a8b39b33ac157
NXXR <thenexxorproject@gmail.com>
2019-05-10 11:18:57 +0900

    fixed bug (sys.argv[1])

--- docs/ProjectReports/WeeklyReports/pcreate.py
+++ docs/ProjectReports/WeeklyReports/pcreate.py
@@ -6,16 +6,15 @@ import glob
 
 if len(sys.argv) > 1:
     for arg in sys.argv[1:]:
-        if os.path.isfile(sys.argv[1]):
-            os.system("pandoc -t slidy -s " + sys.argv[1] + " -o " + sys.argv[1][:-3] + ".html --self-contained --metadata pagetitle=\""+sys.argv[1][:-3]+"\"")
-        elif len(sys.argv[1]) == 2:
-            number = sys.argv[1]
-            file = glob.glob("Week" + sys.argv[1] + "*.md")
-            os.system("pandoc -t slidy -s " + file[0] + " -o " + file[0][:-3] + ".html --self-contained --metadata pagetitle=\""+file[0][:-3]+"\"")
+        if os.path.isfile(arg):
+            os.system("pandoc -t slidy -s " + arg + " -o " + arg[:-3] + ".html --self-contained --metadata pagetitle=\"" + arg[:-3] + "\"")
+        elif len(arg) == 2:
+            file = glob.glob("Week" + arg + "*.md")
+            os.system("pandoc -t slidy -s " + file[0] + " -o " + file[0][:-3] + ".html --self-contained --metadata pagetitle=\"" + file[0][:-3] + "\"")
         else:
-            print("invalid argument \""+arg+"\" skipping...")
+            print("invalid argument \"" + arg + "\" skipping...")
 else:
     number = input("Week Number (2 digits): ")
     if len(number) == 2 and number.isdigit():
-        file = glob.glob("Week" + sys.argv[1] + "*.md")
+        file = glob.glob("Week" + number + "*.md")
         os.system("pandoc -t slidy -s " + file[0] + " -o " + file[0][:-3] + ".html --self-contained --metadata pagetitle=\""+file[0][:-3]+"\"")
