e19ed90af9f7740838bf3fd6bcb42d20cd3047ff
NXXR <thenexxorproject@gmail.com>
2019-05-31 13:41:02 +0900

    edited script to allow multiple numbers input

--- docs/ProjectReports/WeeklyReports/pcreate.py
+++ docs/ProjectReports/WeeklyReports/pcreate.py
@@ -8,13 +8,19 @@ if len(sys.argv) > 1:
     for arg in sys.argv[1:]:
         if os.path.isfile(arg):
             os.system("pandoc -t slidy -s " + arg + " -o " + arg[:-3] + ".html --self-contained --metadata pagetitle=\"" + arg[:-3] + "\"")
-        elif len(arg) == 2:
+        elif len(arg) == 2 and arg.isdigit():
             file = glob.glob("Week" + arg + "*.md")
             os.system("pandoc -t slidy -s " + file[0] + " -o " + file[0][:-3] + ".html --self-contained --metadata pagetitle=\"" + file[0][:-3] + "\"")
         else:
             print("invalid argument \"" + arg + "\" skipping...")
 else:
-    number = input("Week Number (2 digits): ")
-    if len(number) == 2 and number.isdigit():
-        file = glob.glob("Week" + number + "*.md")
-        os.system("pandoc -t slidy -s " + file[0] + " -o " + file[0][:-3] + ".html --self-contained --metadata pagetitle=\""+file[0][:-3]+"\"")
+    number = input("Week Number (2 digits or x to cancel): ")
+    numlist = number.split()
+    for item in numlist:
+        if len(item) == 2 and item.isdigit():
+            file = glob.glob("Week" + item + "*.md")
+            print("creating slides for Week " + item)
+            os.system("pandoc -t slidy -s " + file[0] + " -o " + file[0][:-3] + ".html --self-contained --metadata pagetitle=\"" + file[0][:-3] + "\"")
+            print("DONE")
+        else:
+            print("invalid argument \"" + item + "\" skipping...")
