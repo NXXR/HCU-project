afecd6fef5776f93ceb988c844b09f48f493ba05
NXXR <thenexxorproject@gmail.com>
2019-06-12 18:59:00 +0900

    rebuilt venv for python 64bit

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\M.Zeumer\Workspace\HCU-project\venv"
+$env:VIRTUAL_ENV="C:\Users\m_zeu\PycharmProjects\HCU-project\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
