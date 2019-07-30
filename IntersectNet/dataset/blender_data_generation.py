78fe33607b9f59f22ec27411aa35f3783b3011fa
NXXR <thenexxorproject@gmail.com>
2019-07-30 19:00:29 +0900

    removed floors on corridors and centerpiece

--- IntersectNet/dataset/blender_data_generation.py
+++ IntersectNet/dataset/blender_data_generation.py
@@ -11,7 +11,6 @@ center_options = {
                 (2, -1, 2.5), (2, 1, 2.5), (-2, 1, 2.5), (-2, -1, 2.5)
             ],
             "faces": [
-                (0, 1, 2, 3),  # Floor face
                 (4, 5, 6, 7),  # Ceiling face
                 (1, 2, 6, 5), (3, 0, 4, 7)  # Wall faces
             ]
@@ -22,7 +21,6 @@ center_options = {
                 (2, -1, 2.5), (2, 1, 2.5), (-2, 1, 2.5), (-2, -1, 2.5)
             ],
             "faces": [
-                (0, 1, 2, 3),  # Floor face
                 (4, 5, 6, 7),  # Ceiling face
                 (1, 2, 6, 5), (3, 0, 4, 7)  # Wall faces
             ]
@@ -36,7 +34,6 @@ center_options = {
                 (-2, -1, 2.5)
             ],
             "faces": [
-                (0, 1, 2, 3, 4, 5, 6, 7),  # Floor face
                 (8, 9, 10, 11, 12, 13, 14, 15),  # Ceiling face
                 (1, 2, 10, 9), (2, 3, 11, 10), (4, 5, 13, 12), (5, 6, 14, 13), (7, 0, 8, 15)  # Wall faces
             ]
@@ -47,7 +44,6 @@ center_options = {
                 (2, -1, 2.5), (2, 1, 2.5), (1, 2, 2.5), (-1, 2, 2.5), (-2, 1, 2.5), (-2, -1, 2.5)  # Top verts
             ],
             "faces": [
-                (0, 1, 2, 3, 4, 5),  # Floor faces
                 (6, 7, 8, 9, 10, 11),  # Ceiling faces
                 (1, 2, 8, 7), (3, 4, 10, 9), (5, 0, 6, 11)  # Wall faces
             ]
@@ -62,7 +58,6 @@ center_options = {
                 (-2, -1, 2.5), (-1, -1, 2.5), (-1, -2, 2.5), (1, -2, 2.5), (1, -1, 2.5)
             ],
             "faces": [
-                (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11),  # Floor face
                 (12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23),  # Ceiling face
                 (1, 2, 14, 13), (2, 3, 15, 14), (4, 5, 17, 16), (5, 6, 18, 17), (7, 8, 20, 19), (8, 9, 21, 20),
                 (10, 11, 23, 22), (11, 0, 12, 23)  # Wall faces
@@ -76,7 +71,6 @@ center_options = {
                 (-1, -2, 2.5), (1, -2, 2.5), (1, -1, 2.5)  # Top verts
             ],
             "faces": [
-                (0, 1, 2, 3, 4, 5, 6, 7, 8, 9),  # Floor faces
                 (10, 11, 12, 13, 14, 15, 16, 17, 18, 19),  # Ceiling faces
                 (1, 2, 12, 11), (3, 4, 14, 13), (5, 6, 16, 15), (6, 7, 17, 16), (8, 9, 19, 18), (9, 0, 10, 19)
                 # Wall faces
@@ -102,7 +96,7 @@ def create_random_centerpiece(connections: int):
         mesh.from_pydata(center_options[connections][option]["verts"], [], center_options[connections][option]["faces"])
         mesh.update(calc_edges=True)
         bpy.data.objects["Centerpiece"].rotation_euler = Euler((0, 0, randint(0, 3) * math.radians(90)))
-
+4
 
 # delete all objects in scene
 bpy.ops.object.select_all(action="SELECT")
@@ -114,7 +108,7 @@ bpy.ops.mesh.primitive_plane_add(location=(0, 0, -0.01), radius=10)
 # place corridor X+
 verts = [(2, -1, 0), (8, -1, 0), (8, -1, 2.5), (2, -1, 2.5),
          (2, 1, 0), (8, 1, 0), (8, 1, 2.5), (2, 1, 2.5)]
-faces = [(0, 1, 2, 3), (4, 5, 6, 7), (2, 3, 7, 6), (0, 1, 5, 4)]
+faces = [(0, 1, 2, 3), (4, 5, 6, 7), (2, 3, 7, 6)]
 
 mesh = bpy.data.meshes.new("Xpos")
 obj = bpy.data.objects.new("Xpos", mesh)
