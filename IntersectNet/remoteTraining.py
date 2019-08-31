8d41efdc8376409719319db3785cc04a1c846ea4
NXXR <thenexxorproject@gmail.com>
2019-08-31 15:15:19 +0900

    fixed network input

--- IntersectNet/remoteTraining.py
+++ IntersectNet/remoteTraining.py
@@ -12,7 +12,7 @@ epochs = 60
 
 model = models.Sequential()
 
-model.add(layers.Conv2D(32, (3, 3), activation="relu", input_shape=(dataset_width, dataset_height, 3)))
+model.add(layers.Conv2D(32, (3, 3), activation="relu", input_shape=(dataset_height, dataset_width, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
 
 model.add(layers.Conv2D(64, (3, 3), activation="relu"))
